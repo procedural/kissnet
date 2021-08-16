c25a83035ef2ba0c04ac95c836fcb54e3bb5b886
procedural <iamvfx@gmail.com>
2021-08-16 14:51:33 +0300

    Styling

--- kissnet.h
+++ kissnet.h
@@ -4,8 +4,8 @@
 extern "C" {
 #endif
 
-void kissnetSend(uint64_t sessionId, uint64_t dataBytesCount, const void * data);
-void kissnetReceive(uint64_t sessionId, uint64_t * outDataBytesCount, void * outData);
+void kissnetSend    (uint64_t sessionId, uint64_t dataBytesCount, const void * data);
+void kissnetReceive (uint64_t sessionId, uint64_t * outDataBytesCount, void * outData);
 
 #ifdef __cplusplus
 }
